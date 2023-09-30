9df35cabc048fadb492dfe13a00f186174d86a34
ElezthemDev <97851451+Elezthem@users.noreply.github.com>
2023-09-30 14:25:52 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+enum Options {
+	open = 3,
+	close,
+	wait = 5,
+	del
+};
+
+struct Folder {
+	int weight;
+	string name;
+	Options options;
+};
+
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	Folder my_folder;
+	my_folder.weight = 2.5f;
+	my_folder.name = "Elezthcord.py";
+	my_folder.options = Options::close;
+
+	// cout << my_folder.options;
+	if (my_folder.options == Options::close)
+		cout << "Folder is close" << endl;
+
+	return 0;
+}
